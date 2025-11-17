d4cab704b2ebd80a26a84dfc84a86cff98d78f2d
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2025-11-17 17:03:15 +0000

    chore: Clean up temporary test files

--- test_3hfz.rs
+++
@@ -1,9 +0,0 @@
-﻿use yaml_lib::{parse_yaml_string};
-
-fn main() {
-    let yaml = "---\nkey: value\n... invalid\n";
-    match parse_yaml_string(yaml) {
-        Ok(doc) => println!("SUCCESS (should have failed!): {:?}", doc),
-        Err(e) => println!("ERROR (correct!): {}", e),
-    }
-}
