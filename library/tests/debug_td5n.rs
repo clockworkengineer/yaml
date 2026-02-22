263ec5f47f590055748867427881cdd82e505228
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2026-02-22 07:16:17 +0000

    remov euncessary files

--- library/tests/debug_td5n.rs
+++
@@ -1,7 +0,0 @@
-use yaml_lib::test_helpers::assert_parse_error;
-
-#[test]
-fn inspect_td5n_node_shape() {
-    let input = b"- item1\n- item2\ninvalid\n";
-    assert_parse_error(input, "Unexpected plain scalar after top-level sequence");
-}
